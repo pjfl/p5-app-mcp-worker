faf723fa411fbad9d68484fc2856f9dcf96e4613
pjfl <github@roxsoft.co.uk>
2013-11-02 23:36:25 +0000

    0.2 Added user authentication

--- lib/App/MCP/Worker.pm
+++ lib/App/MCP/Worker.pm
@@ -1,25 +1,24 @@
-# @(#)$Ident: Worker.pm 2013-10-27 23:28 pjf ;
+# @(#)$Ident: Worker.pm 2013-10-31 23:06 pjf ;
 
 package App::MCP::Worker;
 
 use 5.010001;
 use namespace::sweep;
-use version;    our $VERSION = qv( sprintf '0.2.%d', q$Rev: 2 $ =~ /\d+/gmx );
+use version;    our $VERSION = qv( sprintf '0.2.%d', q$Rev: 3 $ =~ /\d+/gmx );
 
 use Class::Usul::Constants;
 use Class::Usul::Crypt         qw( decrypt encrypt );
-use Class::Usul::Functions     qw( base64_decode_ns bson64id
-                                   exception pad throw );
-use Crypt::Eksblowfish::Bcrypt qw( bcrypt en_base64 );
+use Class::Usul::Functions     qw( bson64id exception pad throw );
+use Crypt::Eksblowfish::Bcrypt qw( bcrypt );
 use Cwd                        qw( getcwd );
 use English                    qw( -no_match_vars );
 use File::DataClass::Types     qw( ArrayRef Directory HashRef NonEmptySimpleStr
-                                   NonZeroPositiveInt SimpleStr Str );
+                                   NonZeroPositiveInt Object SimpleStr Str );
 use HTTP::Request::Common      qw( POST );
 use LWP::UserAgent;
 use Moo;
 use MooX::Options;
-use Storable                   qw( nfreeze thaw );
+use JSON                       qw( );
 use TryCatch;
 use Type::Utils                qw( as coerce from subtype via );
 
@@ -31,62 +30,69 @@ my $ServerList = subtype as ArrayRef;
 
 coerce $ServerList, from Str, via { [ split SPC, $_ ] };
 
-option 'job'       => is => 'ro', isa => HashRef,
+option 'job'       => is => 'ro',   isa => HashRef,
    documentation   => 'Keys and values of a job definition in JSON format',
    default         => sub { {} }, json => TRUE, short => 'j';
 
-option 'port'      => is => 'ro', isa => NonZeroPositiveInt, default => 2012,
+option 'port'      => is => 'ro',   isa => NonZeroPositiveInt, default => 2012,
    documentation   => 'Port number for the remote servers. Defaults to 2012',
    format          => 'i', short => 'p';
 
-option 'protocol'  => is => 'ro', isa => NonEmptySimpleStr, default => 'http',
+option 'protocol'  => is => 'ro',   isa => NonEmptySimpleStr, default => 'http',
    documentation   => 'Which network protocol to use. Defaults to http',
    format          => 's', short => 'P';
 
-option 'servers'   => is => 'ro', isa => $ServerList, default => 'localhost',
+option 'servers'   => is => 'ro',   isa => $ServerList, default => 'localhost',
    documentation   => 'List of servers to send request to',
    coerce          => $ServerList->coercion, format => 's', short => 's';
 
-option 'user_name' => is => 'ro', isa => NonEmptySimpleStr,
+option 'user_name' => is => 'ro',   isa => NonEmptySimpleStr,
    documentation   => 'Name in the user table and .mcprc file',
    default         => 'unknown', format => 's', short => 'u';
 
-has 'command'      => is => 'ro', isa => NonEmptySimpleStr, default => 'true';
+has 'command'      => is => 'ro',   isa => NonEmptySimpleStr, default => 'true';
 
-has 'directory'    => is => 'ro', isa => Directory | SimpleStr;
+has 'directory'    => is => 'ro',   isa => Directory | SimpleStr;
 
-has 'job_id'       => is => 'ro', isa => NonZeroPositiveInt, default => $PID;
+has 'job_id'       => is => 'ro',   isa => NonZeroPositiveInt, default => $PID;
 
-has 'runid'        => is => 'ro', isa => NonEmptySimpleStr, default => bson64id;
+has 'runid'        => is => 'ro',   isa => NonEmptySimpleStr,
+   default         => bson64id;
 
-has 'token'        => is => 'ro', isa => SimpleStr;
+has 'token'        => is => 'ro',   isa => SimpleStr;
 
-has 'ev_uri_fmt'   => is => 'ro', isa => NonEmptySimpleStr,
+has 'ev_uri_fmt'   => is => 'ro',   isa => NonEmptySimpleStr,
    default         => 'api/event/%s';
 
-has 'job_uri_fmt'  => is => 'ro', isa => NonEmptySimpleStr,
+has 'job_uri_fmt'  => is => 'ro',   isa => NonEmptySimpleStr,
    default         => 'api/job/%s';
 
-has 'sess_uri_fmt' => is => 'ro', isa => NonEmptySimpleStr,
+has 'sess_uri_fmt' => is => 'ro',   isa => NonEmptySimpleStr,
    default         => 'api/session/%s';
+
+has '_transcoder'  => is => 'lazy', isa => Object,
+   builder         => sub { JSON->new }, reader => 'transcoder';
+
+has '_user_agent'  => is => 'lazy', isa => Object,
+   builder         => sub { LWP::UserAgent->new }, reader => 'user_agent';
 
 # Public methods
 sub create_job : method {
    my $self    = shift;
-   my $ua      = LWP::UserAgent->new;
-   my $sess    = $self->_get_authenticated_session( $ua ) or return FAILED;
+   my $json    = $self->transcoder;
+   my $server  = $self->servers->[ 0 ];
+   my $uri     = $self->protocol."://${server}:".$self->port.'/';
+   my $sess    = $self->_get_authenticated_session( $uri ) or return FAILED;
    my $sess_id = $sess->{id};
-   my $job     = encrypt $sess->{token}, nfreeze $self->job;
-   my $uri     = $self->protocol.'://'.$self->servers->[ 0 ].':'.$self->port;
-      $uri    .= '/'.sprintf $self->job_uri_fmt, $sess_id;
-   my $res     = $ua->request( POST $uri, [ job => $job ] );
-   my $prefix  = " JOB[${sess_id}]: ";
+      $uri    .= sprintf $self->job_uri_fmt, $sess_id;
+   my $job     = encrypt $sess->{token}, $json->encode( $self->job );
+   my $res     = $self->_post_as_json( $uri, { job => $job } );
+   my $content = $json->decode( $res->content );
+   my $message = " JOB[${sess_id}]: ".$res->code.SPC.$content->{message};
 
-   unless ($res->is_success) {
-      $self->error( $prefix.$res->message ); return FAILED;
-   }
+   unless ($res->is_success) { $self->error( $message ); return FAILED }
 
-   $self->debug and $self->log->debug( $prefix.$res->message );
+   $self->info( $message );
    return OK;
 }
 
@@ -104,33 +110,41 @@ sub set_client_password : method {
 
 # Private methods
 sub _get_authenticated_session {
-   my ($self, $ua, $opts) = @_; $opts //= {};
+   my ($self, $uri, $opts) = @_; $opts //= {};
 
+   my $json      = $self->transcoder;
    my $user_name = $opts->{user_name} || $self->user_name;
-   my $uri       = $self->protocol.'://'.$self->servers->[ 0 ].':'.$self->port
-                   .'/'.sprintf $self->sess_uri_fmt, $user_name;
-   my $res       = $ua->request( POST $uri, [] );
+      $uri      .= sprintf $self->sess_uri_fmt, $user_name;
+   my $res       = $self->_post_as_json( $uri, {} );
+   my $content   = $json->decode( $res->content );
 
    unless ($res->is_success) {
-      $self->error( $res->code == 404 ? $res->content : $res->message );
+      $self->error( $res->code == 404 ? $content->{message} : $res->message );
       return FALSE;
    }
 
    my $password  = $opts->{password} || $self->get_user_password( $user_name );
-   my $content   = thaw base64_decode_ns $res->content;
    my $key       = bcrypt( $password, $content->{salt} );
    my $session;
 
-   try        { $session = thaw decrypt $key, $content->{token} }
+   try        { $session = $json->decode( decrypt $key, $content->{token} ) }
    catch ($e) {
-      $self->error( exception error => 'User [_1] authentication failure',
-                              args  => [ $user_name ] );
+      my $message = 'User [_1] authentication failure';
+
+      $self->error( exception error => $message, args  => [ $user_name ] );
       return FALSE;
    }
 
    $self->debug and $self->log->debug
-      ( "User ${user_name} Session Id ".$session->{id}.' Code '.$res->code );
+      ( $res->code." User ${user_name} Session-Id ".$session->{id} );
    return $session;
+}
+
+sub _post_as_json {
+   my ($self, $uri, $content) = @_; my $ua = $self->user_agent;
+
+   return $ua->request( POST $uri, 'Content-Type' => 'application/json',
+                        content => $self->transcoder->encode( $content ) );
 }
 
 sub _run_command {
@@ -150,7 +164,7 @@ sub _send_event {
    my ($self, $transition, $r) = @_;
 
    my $runid   = $self->runid;
-   my $ua      = LWP::UserAgent->new;
+   my $json    = $self->transcoder;
    my $event   = { job_id => $self->job_id, pid        => $PID,
                    runid  => $runid,        transition => $transition, };
    my $prefix  = (pad uc $transition, 9, SPC, 'left')."[${runid}]: ";
@@ -158,17 +172,17 @@ sub _send_event {
 
    $r and $event->{rv} = $r->rv;
    $self->debug and $self->log->debug( $message );
-   $event = encrypt $self->token, nfreeze $event;
+   $event = encrypt $self->token, $json->encode( $event );
 
    for my $server (@{ $self->servers }) {
-      my $uri  = $self->protocol."://${server}:".$self->port.'/';
-         $uri .= sprintf $self->ev_uri_fmt, $self->runid;
-      my $res  = $ua->request( POST $uri, [ event => $event ] );
+      my $uri     = $self->protocol."://${server}:".$self->port.'/';
+         $uri    .= sprintf $self->ev_uri_fmt, $runid;
+      my $res     = $self->_post_as_json( $uri, { event => $event } );
+      my $content = $json->decode( $res->content );
+      my $message = $prefix.$res->code.SPC.$content->{message};
 
-      if ($res->is_success) {
-         $self->debug and $self->log->debug( "${prefix}Code ".$res->code );
-      }
-      else { $self->log->error( $prefix.$res->message ) }
+      unless ($res->is_success) { $self->log->error( $message ) }
+      else { $self->debug and $self->log->debug( $message ) }
    }
 
    return;
@@ -195,7 +209,7 @@ App::MCP::Worker - Remotely executed worker process
 
 =head1 Version
 
-This documents version v0.2.$Rev: 2 $
+This documents version v0.2.$Rev: 3 $
 
 =head1 Synopsis
 
